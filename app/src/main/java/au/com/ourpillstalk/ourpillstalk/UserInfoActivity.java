0a65aadb1bbd90f97e1e403c86b1df75bee3fbb3
Michael Faro-Tusino <dev.mfarotusino@live.com.au>
2015-05-05 14:55:43 +1000

    User Info Updated
    User Info updated to add Allergy field, as well as rename City and
    Suburb fields to City/Suburb and State respectively

--- app/src/main/java/au/com/ourpillstalk/ourpillstalk/UserInfoActivity.java
+++ app/src/main/java/au/com/ourpillstalk/ourpillstalk/UserInfoActivity.java
@@ -14,7 +14,7 @@ import android.widget.EditText;
 public class UserInfoActivity extends AppCompatActivity implements View.OnClickListener{
     Button userInfoButton, privButton;
 
-    EditText nameEditText, cityEditText, suburbEditText, emergencyNumberEditText, medicationsEditText;
+    EditText nameEditText, cityEditText, stateEditText, emergencyNumberEditText, medicationsEditText, allergiesEditText;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTitle("Welcome to Our Pills Talk!");
@@ -25,9 +25,10 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
         }
         nameEditText = (EditText) findViewById(R.id.nameEditText);
         cityEditText = (EditText) findViewById(R.id.cityEditText);
-        suburbEditText = (EditText) findViewById(R.id.suburbEditText);
+        stateEditText = (EditText) findViewById(R.id.stateEditText);
         emergencyNumberEditText = (EditText) findViewById(R.id.emrgencyContactEditText);
         medicationsEditText = (EditText) findViewById(R.id.currentMedicationEditText);
+        allergiesEditText = (EditText) findViewById(R.id.allergyText);
         userInfoButton = (Button) findViewById(R.id.userInfoButton);
         privButton = (Button) findViewById(R.id.privButton);
         setOnClickListeners();
@@ -46,7 +47,7 @@ public class UserInfoActivity extends AppCompatActivity implements View.OnClickL
         switch(view.getId()){
             case(R.id.userInfoButton): {
                 SharedPreferencesIO.setNewUser(false, getApplicationContext());
-                String[] userInfo = {nameEditText.getText().toString(), cityEditText.getText().toString(), suburbEditText.getText().toString(), emergencyNumberEditText.getText().toString(), medicationsEditText.getText().toString()};
+                String[] userInfo = {nameEditText.getText().toString(), cityEditText.getText().toString(), stateEditText.getText().toString(), emergencyNumberEditText.getText().toString(), medicationsEditText.getText().toString(), allergiesEditText.getText().toString()};
                 FileIO.saveUserInformation(userInfo, getApplicationContext());
                 Intent showMain = new Intent(this, MainActivity.class);
                 startActivity(showMain);
